daec7f54939babad528a73b649ace3669cd04a18
유주현 <dbwngus08@naver.com>
2025-03-13 00:18:51 +0900

    Solve 1920

---
+++ src/baekjoon/hashmap/Solution1920.java
@@ -0,0 +1,33 @@
+package baekjoon.hashmap;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Solution1920 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        Set<Integer> set = new HashSet<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            set.add(Integer.parseInt(st.nextToken()));
+        }
+
+        int m = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            int res = set.contains(Integer.parseInt(st2.nextToken())) ? 1 : 0;
+            sb.append(res).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}

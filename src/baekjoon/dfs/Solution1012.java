7fc9fe744f2fccc67654862b83f513cca4b623d6
유주현 <dbwngus08@naver.com>
2025-03-15 16:27:50 +0900

    Solve 1012 Using dfs

---
+++ src/baekjoon/dfs/Solution1012.java
@@ -0,0 +1,68 @@
+package baekjoon.dfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution1012 {
+    static int M, N;
+    static int[][] graph;
+    static boolean[][] visited;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            M = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+            int K = Integer.parseInt(st.nextToken());
+
+            graph = new int[M][N];
+            visited = new boolean[M][N];
+
+            for (int i = 0; i < K; i++) {
+                st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+
+                graph[x][y] = 1;
+            }
+
+            int count = 0;
+            for (int i = 0; i < M; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (graph[i][j] == 1 && !visited[i][j]) {
+                        dfs(i, j);
+                        count++;
+                    }
+                }
+            }
+            sb.append(count).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    static void dfs(int x, int y) {
+        visited[x][y] = true;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx >= 0 && ny >= 0 && nx < M && ny < N) {
+                if (graph[nx][ny] == 1 && !visited[nx][ny]) {
+                    dfs(nx, ny);
+                }
+            }
+        }
+
+    }
+}

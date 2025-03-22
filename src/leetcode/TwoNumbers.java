71347cb0187b2d9de1420a46d0622c02c0a31796
유주현 <dbwngus08@naver.com>
2025-03-22 16:59:04 +0900

    Solve LeetCode 2. Add Two Numbers

---
+++ src/leetcode/TwoNumbers.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+public class TwoNumbers {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode curr = head;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int val1 = (l1 != null) ? l1.val : 0;
+            int val2 = (l2 != null) ? l2.val : 0;
+
+            int sum = val1 + val2 + carry;
+            carry = sum / 10;
+            int digit = sum % 10;
+
+            curr.next = new ListNode(digit);
+            curr = curr.next;
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+
+        return head.next;
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+}

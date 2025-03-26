35ee6244433fe1343097062f15f8640b5f91e679
유주현 <dbwngus08@naver.com>
2025-03-27 00:31:45 +0900

    Solve Leet Code Median of Two Sorted Arrays

--- src/leetcode/TwoSortedArrays.java
+++ src/leetcode/TwoSortedArrays.java
@@ -1,31 +1,40 @@
 package leetcode;
 
-import java.util.Arrays;
-
 public class TwoSortedArrays {
-    public static void main(String[] args) {
-        int[] nums1 = {1, 3};
-        int[] nums2 = {2};
-
-        int len1 = nums1.length;
-        int len2 = nums2.length;
-
-        int[] nums3 = new int[len1 + len2];
-        System.arraycopy(nums1, 0, nums3, 0, len1);
-        System.arraycopy(nums2, 0, nums3, len1, len2);
-
-        Arrays.sort(nums3);
-
-        int len3 = nums3.length;
-
-        double res = 0;
-
-        if (len3 % 2 == 0) {
-            res = (double) (nums3[len3 / 2] + nums3[(len3 / 2) - 1]) / 2;
-        } else {
-            res = nums3[(len3 - 1) / 2];
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        if (nums1.length > nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
         }
 
-        System.out.println(res);
+        int x = nums1.length;
+        int y = nums2.length;
+
+        int low = 0;
+        int high = x;
+
+        while (low <= high) {
+            int partitionX = (low + high) / 2;
+            int partitionY = (x + y + 1) / 2 - partitionX;
+
+            int maxLeftX = (partitionX == 0) ? Integer.MIN_VALUE : nums1[partitionX - 1];
+            int minRightX = (partitionX == x) ? Integer.MAX_VALUE : nums1[partitionX];
+
+            int maxLeftY = (partitionY == 0) ? Integer.MIN_VALUE : nums2[partitionY - 1];
+            int minRightY = (partitionY == y) ? Integer.MAX_VALUE : nums2[partitionY];
+
+            if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
+                if ((x + y) % 2 == 0) {
+                    return ((double) Math.max(maxLeftX, maxLeftY) + Math.min(minRightX, minRightY)) / 2;
+                } else {
+                    return (double) Math.max(maxLeftX, maxLeftY);
+                }
+            } else if (maxLeftX > minRightY) {
+                high = partitionX - 1;
+            } else {
+                low = partitionX + 1;
+            }
+        }
+
+        return -1;
     }
 }
